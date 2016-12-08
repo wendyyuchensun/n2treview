aeb977ea92be7c5fe33ee186b70384c81b316223
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2016-12-09 00:11:35 +0800

    Add interative file-loading interface

--- assembler/Assembler.js
+++ assembler/Assembler.js
@@ -3,9 +3,10 @@ var clear = require('./clear.js');
 var st = require('./st.js');
 var decode = require('./decode.js');
 var cMap = require('./cMap.js')
+var argv = require('yargs').argv;
 
 // Open the file
-fs.readFile(__dirname + '/Max.asm', (err, data) => {
+fs.readFile(__dirname + '/' + argv.f + '.asm', (err, data) => {
   if (err) throw err;
   var rawDArr = data.toString()
                     .split('\n');
@@ -13,7 +14,7 @@ fs.readFile(__dirname + '/Max.asm', (err, data) => {
   var sTable = st.st(clearedDArr);
   var decodedDArr = decode.decode(clearedDArr, sTable, cMap);
   var newD = decodedDArr.join('\n');
-  fs.writeFile('./Max.hack', newD, (err) => {
+  fs.writeFile('./' + argv.f  + '.hack', newD, (err) => {
     if (err) throw err;
     console.log('Success!');
   });
