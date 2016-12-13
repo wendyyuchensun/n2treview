c7b874a7d347514951932cb4e9fc494c5b783a7c
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2016-12-13 13:30:46 +0800

    Restart

--- vm1/modules/clearer.js
+++
@@ -1,7 +0,0 @@
-// Clearer: remove comments, carriage retrun/newline,
-//          and empty space/lines.
-
-module.exports = (command) => {
- command.replace(/\/\/.*/g, '').trim();
- return command;
-};
