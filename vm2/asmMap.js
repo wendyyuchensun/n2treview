0d27599a90db8d42655c5238cbe0e853c8ab3380
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2016-12-14 21:48:41 +0800

    vm2 init commit

---
+++ vm2/asmMap.js
@@ -0,0 +1,83 @@
+var segments = (seg, shift) => {
+  switch(seg) {
+    case 'local': 
+      return 'LCL';
+      break;
+    case 'argument': 
+      return 'ARG';
+      break;
+    case 'this': 
+      return 'THIS';
+      break;
+    case 'that': 
+      return 'THAT';
+      break;
+    case 'temp': 
+      return 'temp';
+      break;
+    case 'pointer':
+      return (shift === '0'? 'THIS':'THAT');
+      break;
+  };
+};
+
+module.exports = {
+  add: '@SP\nAM=M-1\nD=M\nA=A-1\nM=D+M',
+  sub: '@SP\nAM=M-1\nD=M\nA=A-1\nM=M-D',
+  and: '@SP\nAM=M-1\nD=M\nA=A-1\nM=D&M',
+  or: '@SP\nAM=M-1\nD=M\nA=A-1\nM=D|M',
+  neg: '@SP\nA=M-1\nM=-M',
+  not: '@SP\nA=M-1\nM=!M',
+  eq: '@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@EQ.true.\nD;JEQ\n@SP\nA=M-1\nM=0\n@EQ.end.\n0;JMP\n(EQ.true.)\n@SP\nA=M-1\nM=-1\n(EQ.end.)',
+  gt: '@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@GT.true.\nD;JGT\n@SP\nA=M-1\nM=0\n@GT.end.\n0;JMP\n(GT.true.)\n@SP\nA=M-1\nM=-1\n(GT.end.)',
+  lt: '@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@LT.true.\nD;JLT\n@SP\nA=M-1\nM=0\n@LT.end.\n0;JMP\n(LT.true.)\n@SP\nA=M-1\nM=-1\n(LT.end.)',
+  push: (seg, shift, f) => {
+    var t, s, translated;
+    switch (seg) {
+      case 'constant': 
+        t = '@\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1';
+        translated = [t.slice(0, 1), shift, t.slice(1)]
+          .join('');
+        break;
+      case 'temp':
+        t = '@R5\nD=A\n@\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1';
+        translated = [t.slice(0, 9), shift, t.slice(9)].join('');
+        break;
+      case 'pointer':
+        t = '@R3\nD=A\n@\nD=D+A\nA=D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1';
+        translated = [t.slice(0, 9), shift, t.slice(9)].join('');
+        break;
+      case 'static':
+        t = '@.\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1';
+        translated = [t.slice(0, 1), f, t.slice(1, 2), shift, t.slice(2)].join('');
+        break;
+      default: 
+        t = '@\nD=M\n@\nD=D+A\nA=D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1';
+        s = segments(seg, shift);
+        translated = [t.slice(0, 1), s, t.slice(1, 7), shift, t.slice(7)].join('');
+   };
+   return translated;
+ },
+ pop: (seg, shift, f) => {
+   var t, translated;
+   switch (seg) {
+    case 'static':
+      t = '@SP\nAM=M-1\nD=M\n@.\nM=D';
+      translated = [t.slice(0, 23), f, t.slice(23, 24), shift, t.slice(24)].join('');
+      break;
+    case 'temp':
+      t = '@R5\nD=A\n@\nD=A+D\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D';
+      translated = [t.slice(0, 9), shift, t.slice(9)].join('');
+      break;
+    case 'pointer':
+      t = '@R3\nD=A\n@\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D';
+      translated = [t.slice(0, 9), shift, t.slice(9)].join('');
+      break;
+    default:
+      t = '@\nD=M\n@\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D';
+      s = segments(seg, shift);
+      translated = [t.slice(0, 1), s, t.slice(1, 7), shift, t.slice(7)].join('');
+  };
+   return translated;
+ } 
+}
---
+++ vm2/vmTranslator.js
@@ -0,0 +1,69 @@
+var fs = require('fs');
+var argv = require('yargs').argv;
+var asmMap = require('./asmMap.js');
+
+// Open file and turn it into an array
+// with every line made into an item
+fs.readFile(__dirname + '/' + argv.f + '.vm', (err, data) => {
+  if (err) throw err;
+  var dArr = data.toString().split(/\n/);
+  // Clear unwanted parts
+  var clearedDArr = [];
+  dArr.forEach((item) => {
+    item = item.replace(/^\/\/.*/g, '').replace(/\r/g, '').trim();
+    if (item) {
+      clearedDArr.push(item);
+    };
+  });
+  // Translate
+  var translatedDArr = [];
+  var order = {
+    eq: 0,
+    gt: 0,
+    lt: 0
+  };
+  function insertOrder (t, order) {
+    return [t.slice(0, 36), order, t.slice(36, 65),
+      order, t.slice(65, 81), order, t.slice(81, 106),
+      order, t.slice(106)]
+      .join('');
+  };
+  function msL (item) {
+    item = item.replace('push', '')
+               .replace('pop', '')
+               .trim();
+    var seg = item;
+    seg = seg.replace(/\d+/g, '').trim();
+    var s = item.replace(seg, '').trim();
+    var shift = (s === ''? '0':s);
+    var location = {
+      seg: seg,
+      shift: shift
+    };
+    return location;
+  };
+  clearedDArr.forEach((item) => {
+    var translated;
+    if (item.indexOf('push') === -1 
+        && item.indexOf('pop') === -1) {
+      translated = asmMap[item];
+      if (order.hasOwnProperty(item)) {
+        translated = insertOrder(translated, order[item]);
+        order[item]++;
+      };
+    } else {
+      var l = msL(item);
+      if (item.indexOf('push') > -1) {
+        translated = asmMap.push(l.seg, l.shift, argv.f.toLowerCase());
+      } else {
+        translated = asmMap.pop(l.seg, l.shift, argv.f.toLowerCase());
+      }; 
+    };
+    translatedDArr.push(translated);
+  });
+  var newHack = translatedDArr.join('\n');
+  fs.writeFile(__dirname + '/' + argv.f + '.asm', newHack, (err) => {
+    if (err) throw err;
+    console.log('File saved.')
+  })
+})
